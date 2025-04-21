a1840b335917eda8a2ccfc0b7dab11326013dc51
Mostafa Al Moin <sa479374@gmail.com>
2025-04-21 16:35:34 +0600

    Quick Sort

---
+++ Sorting Algorithms/Quick Sort.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high) {
+
+    int i = low;
+    int j = high;
+    int pivot = arr[low];
+
+    while (i < j) {
+
+        while (arr[i] <= pivot && i < high) i++;
+
+        while (arr[j] > pivot && j > low) j--;
+
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[j], arr[low]);
+
+    return j;
+}
+
+void quickSort(vector<int> &arr, int low, int high) {
+
+    if (low < high) {
+
+        int pIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pIndex - 1);
+        quickSort(arr, pIndex + 1, high);
+
+    }
+
+}
+
+int main() {
+
+    int n; cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
